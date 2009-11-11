cfd8c643d1d0c4f466a2fd7d45ee7e5036ee51dd
U-PHILL\p davies <p davies@PHILL.(none)>
2009-11-11 10:29:15 -0500

    Fixing attribute table handling

--- ppu_helper.rb
+++ ppu_helper.rb
@@ -5,7 +5,7 @@ module PPUHelper
   PATTERN_TABLE_BIT_MASK = [0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01]
   PATTERN_TABLE_BYTE1_BIT_SHIFT = [7, 6, 5, 4, 3, 2, 1, 0]
   PATTERN_TABLE_BYTE2_BIT_SHIFT = [6, 5, 4, 3, 2, 1, 0, -1]
-  ATTRIBUTE_TABLE_BIT_MASK = [0x03, 0x0A, 0x30, 0xA0]
+  ATTRIBUTE_TABLE_BIT_MASK = [0x03, 0x0C, 0x30, 0xC0]
   ATTRIBUTE_TABLE_BIT_SHIFT = [-2, 0, 2, 4]
 
   def initialize
@@ -24,7 +24,10 @@ module PPUHelper
   end
 
   def get_attribute_table_index(tile_index)
-    return ((tile_index / 32).floor * 8) + ((tile_index / 4).floor)
+    result = 0
+    result += ((tile_index / 128).floor * 8)  # Row of attribute table 'grids'
+    result += ((tile_index % 32) / 4).floor
+    return result
   end
 
   def get_attribute_table_square(tile_index)
